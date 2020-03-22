2c709e204bda0cfa657d7070c10fcd2b75f148a3
xiaobeast <243487627@qq.com>
2020-03-22 17:30:50 +0800

    使用通用视图完成views.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from . import views
 
+app_name = 'polls'  # namespace命名空间URL名称，这样就可以在url中通过polls：xxx.html的方式来使用模板
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^$', views.IndexView.as_view(), name='index'),  # /polls
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),  # name参数可以通过{% url %}标签调用
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
