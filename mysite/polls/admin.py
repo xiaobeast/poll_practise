e1d203ab7cfeabf89c003d35c00c18c742eb9087
xiaobeast <243487627@qq.com>
2020-03-23 15:24:13 +0800

    完成初级教程

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from .models import Question, Choice
 # Register your models here.
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    # 通过list_display来改变Questions的陈列方式
+    list_display = (
+        'question_text', 'pub_date', 'was_published_recently'
+    )
+    # 通过search_fields来增加搜索条
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -19,17 +20,27 @@ from .models import Question, Choice
 # def results(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, 'polls/results.html', {'question':question})
+
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
